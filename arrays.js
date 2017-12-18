d7171414d498b26af2b4d3e43049675d283fd245
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-18 20:01:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ var add = "Almond Joy"
 function addElementToBeginningOfArray(chocolateBars,add){
   return [add, ...chocolateBars]
 }
+function destructivelyAddElementToBeginningOfArray(chocolateBars,add){
+  return chocolateBars.unshift(add)
+}
